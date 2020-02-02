9c5bfec0ea4217194d1a986835698eb86447b464
oliverchanyw <oliverchanyw@gmail.com>
2020-02-02 16:05:03 +0800

    one shot

---
+++ src/Reader.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Reader {
+
+    final static String CSVFILE = "/Users/oliverchan/gwg/GWG Commissioning Ball Survey.csv";
+    public final static int LIKE = 10;
+    public final static int DISLIKE = -30;
+    public final static int SECTION = 0;
+    public final static int PLATOON = 0;
+
+    public static Preference read() {
+        BufferedReader br = null;
+        String line;
+        Preference prefs = new Preference();
+
+        try {
+
+            br = new BufferedReader(new FileReader(CSVFILE));
+            while ((line = br.readLine()) != null) {
+
+                String[] splitLine = line.split(","); // name, section, likes, dislikes
+                String name = splitLine[0];
+                int section = Integer.parseInt(splitLine[1]);
+                Set<String> likes = Arrays.stream(splitLine[2].split(";")).collect(Collectors.toSet());
+                Set<String> dislikes = Arrays.stream(splitLine[3].split(";")).collect(Collectors.toSet());
+
+                prefs.parse(name, section, likes, dislikes);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return prefs;
+    }
+
+    public static class Preference {
+        public Map<String, Integer> names;
+        public Map<Integer, String> revNames;
+        public Map<Integer, Integer> sect; // section
+        public Map<IntPair, Integer> prefs; // people preference
+        private int counter;
+
+        public Preference() {
+            names = new HashMap<>();
+            prefs = new HashMap<>();
+            revNames = new HashMap<>();
+            sect = new HashMap<>();
+            counter = 1;
+        }
+
+        public void parse(String name, int section, Set<String> likes, Set<String> dislikes) {
+            int idx = recognize(name);
+            sect.put(idx, section);
+
+            for (String likeable : likes) {
+                if (likeable.equals("Rezo")) continue;
+                addPref(idx, recognize(likeable), LIKE);
+            }
+
+            for (String dislikeable : dislikes) {
+                if (dislikeable.equals("Rezo")) continue;
+                if (dislikeable.equals("No Preference")) continue;
+                addPref(idx, recognize(dislikeable), DISLIKE);
+            }
+        }
+
+        public int getPref(int i, int j) {
+            boolean section = sect.get(i).equals(sect.get(j));
+            boolean platoon = ((3.5 - sect.get(i)) * (3.5 - sect.get(j))) > 0;
+            return prefs.getOrDefault(new IntPair(i, j), section ? SECTION : platoon ? PLATOON : 0);
+        }
+
+        private int recognize(String name) {
+            if (names.containsKey(name)) return names.get(name);
+            else {
+                names.put(name, counter);
+                revNames.put(counter, name);
+                return counter++;
+            }
+        }
+
+        private void addPref(int i, int j, int strength) {
+            IntPair ij = new IntPair(i, j);
+            prefs.put(ij, prefs.getOrDefault(ij, 0) + strength);
+        }
+    }
+
+    public static class IntPair {
+        int i, j;
+
+        public IntPair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof IntPair) {
+                IntPair ip2 = (IntPair) o;
+                return (this.i == ip2.i && this.j == ip2.j) || (this.i == ip2.j && this.j == ip2.i);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return (127 * (i + j)) + (i * j);
+        }
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,134 @@
+import net.sf.javailp.*;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    final static int REPS = 1;
+    final static int TIMEOUT = 100;
+    Problem problem;
+    Reader.Preference prefs;
+
+    public void solveSeating() {
+
+        // Read csv
+        prefs = Reader.read();
+
+        Result best = null;
+
+        for (int rep = 0; rep < REPS; rep++)  {
+            Result result = solveOnce(TIMEOUT);
+            if (best == null || result.getObjective().intValue() > best.getObjective().intValue()) {
+                best = result;
+                displayResult(result);
+            }
+        }
+    }
+
+    private void displayResult(Result result) {
+        Map<Integer, Set<String>> allocation = new HashMap<>();
+        IntStream.range(1, 10).forEach(t -> {
+            allocation.put(t, new HashSet<>());
+            IntStream.range(1, 46).forEach(i -> {
+                if (result.getBoolean(varName("x", t, i))) allocation.get(t).add(prefs.revNames.get(i));
+            });
+        });
+        System.out.println("Solution of utility " + result.getObjective().intValue() + " found");
+        for (Set<String> table : allocation.values()) System.out.println(table);
+    }
+
+    private Result solveOnce(int timeout) {
+        // Choose solver
+        SolverFactory factory = new SolverFactorySAT4J();
+        factory.setParameter(Solver.TIMEOUT, timeout);
+        Solver solver = factory.get();
+
+        // werk
+        setUpObjective();
+        setUpY();
+        setUpX();
+        return solver.solve(problem);
+    }
+    private static Set<Reader.IntPair> allPairs() {
+        Set<Reader.IntPair> pairs = new HashSet<>();
+        for (int i = 1; i <= 45; i++) {
+            for (int j = i + 1; j <= 45; j++) {
+                pairs.add(new Reader.IntPair(i, j));
+            }
+        }
+        return pairs;
+    }
+
+    private static String varName(String prefix, int a, int b, int t) {
+        return prefix + String.format("%02d", a) + String.format("%02d", b) + t;
+    }
+
+    private static String varName(String prefix, int t, int i) {
+        return prefix + t + String.format("%02d", i);
+    }
+
+    // Set up objective - maximize sum(y_abt)
+    private void setUpObjective() {
+        problem = new Problem();
+        Linear objective = new Linear();
+        for (Reader.IntPair pair : allPairs()) {
+            IntStream.range(1, 10)
+                    .forEach(t -> objective.add(prefs.getPref(pair.i, pair.j), varName("y", pair.i, pair.j, t)));
+        }
+        problem.setObjective(objective, OptType.MAX);
+    }
+
+    private void setUpY() {
+        for (int t = 1; t <= 9; t++) {
+            for (Reader.IntPair pair : allPairs()) {
+                // y_abt >= x_ta + x_tb - 1
+                Linear yesyes = new Linear();
+                yesyes.add(1, varName("y", pair.i, pair.j, t));
+                yesyes.add(-1, varName("x", t, pair.i));
+                yesyes.add(-1, varName("x", t, pair.j));
+                problem.add(yesyes, ">=", -1);
+
+                // y_abt <= x_ta && y_abt <= x_tb
+                Linear no1 = new Linear();
+                no1.add(1, varName("y", pair.i, pair.j, t));
+                no1.add(-1, varName("x", t, pair.i));
+                problem.add(no1, "<=", 0);
+
+                Linear no2 = new Linear();
+                no2.add(1, varName("y", pair.i, pair.j, t));
+                no2.add(-1, varName("x", t, pair.j));
+                problem.add(no2, "<=", 0);
+
+                // boolean 0-1, integer constraint
+                problem.setVarType(varName("y", pair.i, pair.j, t), Boolean.class);
+            }
+        }
+    }
+
+    private void setUpX() {
+        // table capacity = 5
+        IntStream.range(1, 10).forEach(t -> {
+            Linear full = new Linear();
+            IntStream.range(1, 46).forEach(i -> full.add(1, varName("x", t, i)));
+            problem.add(full, "=", 5);
+        });
+
+
+        // all cadets seated
+        IntStream.range(1, 46).forEach(i -> {
+            Linear unique = new Linear();
+            IntStream.range(1, 10).forEach(t -> unique.add(1, varName("x", t, i)));
+            problem.add(unique, "=", 1);
+        });
+
+        // boolean 0-1, integer
+        IntStream.range(1, 46).forEach(i -> {
+            IntStream.range(1, 10).forEach(t -> problem.setVarType(varName("x", t, i), Boolean.class));
+        });
+    }
+
+    public static void main(String[] args) {
+        (new Solution()).solveSeating();
+    }
+}
